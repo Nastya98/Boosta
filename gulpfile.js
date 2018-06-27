373eea512ac9c019b8d17f1fe8fb603bfc65e852
Nastya98 <nastyamalinka@ukr.net>
2018-06-27 10:52:51 +0300

    new change

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('app/sass/**/*.scss')
@@ -7,6 +8,14 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('app/css'));
 });
 
+gulp.task('default', () =>
+    gulp.src('app/css/**/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css/'))
+);
 gulp.task('watch', function () {
   gulp.watch('app/sass/**/*.scss', ['sass']);
 });
